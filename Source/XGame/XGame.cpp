9c8f21ad476161816cb0bf572167a9ccfe066d38
hustxiongkun <835288864@qq.com>
2022-08-18 23:32:16 +0800

    【初始化项目】

---
+++ Source/XGame/XGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "XGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, XGame, "XGame" );
