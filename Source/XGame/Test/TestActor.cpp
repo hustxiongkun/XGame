2a453db2906094bb5d1a6f131687305ecdac4d4d
nukxiong <835288864@qq.com>
2022-09-09 16:16:48 +0800

    【调整代码格式】

--- Source/XGame/Test/TestActor.cpp
+++ Source/XGame/Test/TestActor.cpp
@@ -1,17 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TestActor.h"
 
-// Sets default values
 ATestActor::ATestActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false;
 }
 
-// Called when the game starts or when spawned
 void ATestActor::BeginPlay()
 {
 	Super::BeginPlay();
@@ -24,13 +17,15 @@ void ATestActor::BeginPlay()
 			UE_LOG(LogTemp, Log, TEXT("[Test] ObjProperty is valid"));
 			if (const FTestConfig* ObjConfig = ObjProperty->ContainerPtrToValuePtr<FTestConfig>(TestObject))
 			{
-				UE_LOG(LogTemp, Log, TEXT("[Test] ObjConfig is valid: Start = %f,  Location = %s"), ObjConfig->Start, *ObjConfig->Location.ToString());
+				UE_LOG(LogTemp, Log, TEXT("[Test] ObjConfig is valid: Start = %f,  Location = %s"), ObjConfig->Start,
+				       *ObjConfig->Location.ToString());
 			}
 		}
-		if (const FDoubleProperty* ObjFloat = CastField<FDoubleProperty>(TestObject->GetClass()->FindPropertyByName(FName("TestFloat"))))
+		if (const FDoubleProperty* ObjFloat = CastField<FDoubleProperty>(
+			TestObject->GetClass()->FindPropertyByName(FName("TestFloat"))))
 		{
-			UE_LOG(LogTemp, Log, TEXT("[Test] ObjFloat is valid: Float = %f"), ObjFloat->GetPropertyValue_InContainer(TestObject));
-
+			UE_LOG(LogTemp, Log, TEXT("[Test] ObjFloat is valid: Float = %f"),
+			       ObjFloat->GetPropertyValue_InContainer(TestObject));
 		}
 	}
 	if (TestClass)
@@ -39,9 +34,11 @@ void ATestActor::BeginPlay()
 		if (const FProperty* ClassProperty = TestClass->FindPropertyByName(FName("ObjectConfig")))
 		{
 			UE_LOG(LogTemp, Log, TEXT("[Test] ClassProperty is valid"));
-			if (const FTestConfig* ClassConfig = ClassProperty->ContainerPtrToValuePtr<FTestConfig>(TestClass->GetDefaultObject()))
+			if (const FTestConfig* ClassConfig = ClassProperty->ContainerPtrToValuePtr<FTestConfig>(
+				TestClass->GetDefaultObject()))
 			{
-				UE_LOG(LogTemp, Log, TEXT("[Test] ClassConfig is valid: Start = %f,  Location = %s"), ClassConfig->Start, *ClassConfig->Location.ToString());
+				UE_LOG(LogTemp, Log, TEXT("[Test] ClassConfig is valid: Start = %f,  Location = %s"),
+				       ClassConfig->Start, *ClassConfig->Location.ToString());
 			}
 		}
 		if (FProperty* ClassFloat = TestClass->FindPropertyByName(FName("TestFloat")))
@@ -53,19 +50,9 @@ void ATestActor::BeginPlay()
 			}
 			if (const FDoubleProperty* FlotPtr = CastField<FDoubleProperty>(ClassFloat))
 			{
-				UE_LOG(LogTemp, Log, TEXT("[Test] FlotPtr is valid: Float = %f"), FlotPtr->GetPropertyValue_InContainer(TestClass->GetDefaultObject()));
-
+				UE_LOG(LogTemp, Log, TEXT("[Test] FlotPtr is valid: Float = %f"),
+				       FlotPtr->GetPropertyValue_InContainer(TestClass->GetDefaultObject()));
 			}
-
 		}
-
 	}
 }
-
-// Called every frame
-void ATestActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
--- Source/XGame/Test/TestActor.h
+++ Source/XGame/Test/TestActor.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -17,28 +15,25 @@ struct FTestConfig
 	FVector Location = FVector::ZeroVector;
 };
 
-
 UCLASS()
 class XGAME_API ATestActor : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
+	/**
+	 * @brief 构造函数
+	 */
 	ATestActor();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", DisplayName = "配置")
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", meta = (AllowPrivateAccess, DisplayName = "配置"))
 	UObject* TestObject = nullptr;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", DisplayName = "配置类")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", meta = (AllowPrivateAccess, DisplayName = "配置类"))
 	TSubclassOf<UObject> TestClass = nullptr;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", DisplayName = "Actor")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", meta = (AllowPrivateAccess, DisplayName = "测试"))
 	AActor* TestActor = nullptr;
 };
