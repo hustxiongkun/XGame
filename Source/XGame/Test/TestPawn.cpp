8e313d01c386a23ea5ca98a6a01fd7aace3fa564
nukxiong <835288864@qq.com>
2022-10-19 18:53:31 +0800

    【测试绑定payload参数】

---
+++ Source/XGame/Test/TestPawn.cpp
@@ -0,0 +1,24 @@
+﻿#include "TestPawn.h"
+
+ATestPawn::ATestPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATestPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	FInputActionBinding AB("Test", IE_Pressed);
+	AB.ActionDelegate = FInputActionHandlerSignature::CreateUObject(this, &ATestPawn::Test, true);
+	PlayerInputComponent->AddActionBinding(AB);
+	FInputActionBinding AB2("Test2", IE_Pressed);
+	AB2.ActionDelegate = FInputActionHandlerSignature::CreateUObject(this, &ATestPawn::Test, false);
+	PlayerInputComponent->AddActionBinding(AB2);
+}
+
+void ATestPawn::Test(const bool bShowLog)
+{
+	bShow = bShowLog;
+	UE_LOG(LogTemp, Log, TEXT("[Test] ATestPawn::Test ShowLog = %s"), bShowLog ? TEXT("true"): TEXT("false"));
+}
+
---
+++ Source/XGame/Test/TestPawn.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "TestPawn.generated.h"
+
+UCLASS()
+class XGAME_API ATestPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ATestPawn();
+
+protected:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+private:
+	void Test(const bool bShowLog);
+
+	bool bShow = false;
+};
