0c3e3a1b7ce8b3e5a1839eef86263cd92eadad56
nukxiong <835288864@qq.com>
2022-11-10 15:05:25 +0800

    【测试GAS】

---
+++ Source/XGame/Test/TestCharacter.cpp
@@ -0,0 +1,46 @@
+﻿#include "TestCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+ATestCharacter::ATestCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComp = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComp"));
+}
+
+void ATestCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(AbilitySystemComp)
+	{
+		for(const auto& [AbilityClass, Level, InputIds]: AbilityInfos)
+		{
+			for(const auto& InputId : InputIds)
+			{
+				// 创建枚举对应的输入ID到技能的映射
+				AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(AbilityClass, Level, static_cast<int32>(InputId), this));
+			}
+		}
+	}
+}
+
+void ATestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if(AbilitySystemComp)
+	{
+		// 绑定输入到枚举(EAbilityInputId)的映射
+		AbilitySystemComp->BindAbilityActivationToInputComponent(PlayerInputComponent, FGameplayAbilityInputBinds(
+			FString() ,FString(), FString("EAbilityInputId")));
+	}
+}
+
+UAbilitySystemComponent* ATestCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComp;
+}
+
---
+++ Source/XGame/Test/TestCharacter.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameFramework/Character.h"
+#include "TestCharacter.generated.h"
+
+UENUM(BlueprintType)
+enum class EAbilityInputId : uint8
+{
+	None = 0 UMETA(Hidden),
+	Jump,
+	Sprint,
+};
+
+USTRUCT(BlueprintType)
+struct FAbilityInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (DisplayName = "技能类"))
+	TSubclassOf<UGameplayAbility> AbilityClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (DisplayName = "技能等级"))
+	int32 Level = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (DisplayName = "对应ID"))
+	TSet<EAbilityInputId> InputIds;
+};
+
+UCLASS()
+class XGAME_API ATestCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ATestCharacter();
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UAbilitySystemComponent* AbilitySystemComp = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (DisplayName = "技能信息", AllowPrivateAccess = "true"))
+	TArray<FAbilityInfo> AbilityInfos;
+};
