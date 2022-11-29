6595ca6b16a1329a48d7621a3694a90d452a69e4
hustxiongkun <835288864@qq.com>
2022-11-29 21:18:51 +0800

    【小调整】

--- Source/XGame/Test/DerivedActor.h
+++ Source/XGame/Test/DerivedActor.h
@@ -12,6 +12,7 @@ class XGAME_API ADerivedActor : public ATestActor
 public:
 	ADerivedActor();
 
+	// ReSharper disable once CommentTypo
 	// 子类不能使用UFUNCTION()进行标注 
 	virtual void TestBlueprintCallable() override;
 };
